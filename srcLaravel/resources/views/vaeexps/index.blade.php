da645454c6850b3dfaf939e7c09e7717e1b32159
Arnaud <arbph@sfr.fr>
2025-08-11 11:03:19 +0200

    Update index.blade.php

--- srcLaravel/resources/views/vaeexps/index.blade.php
+++ srcLaravel/resources/views/vaeexps/index.blade.php
@@ -1,5 +1,5 @@
 @php
-$keyGOOGLESEARCH = "AfXsb50qf2vbKIfIt9K4j3AdN6a8WHUaGmDdeHcdN0Q";
+$keyGOOGLESEARCH = "KEY;
 $titre = "Elfennel - Module des expériences";
 @endphp
 
@@ -109,3 +109,4 @@ $titre = "Elfennel - Module des expériences";
 	@endforeach
 	</ul>
 @endsection
+
